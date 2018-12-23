6197b64184b7d7a247821a2c769b054d38d64bed
decadebeyond <43082437+decadebeyond@users.noreply.github.com>
2018-12-23 20:47:00 +0800

    Add files via upload

--- JSP网站/src/cn/edu/wic/jsp/servlet/UserToLoginServlet.java
+++ JSP网站/src/cn/edu/wic/jsp/servlet/UserToLoginServlet.java
@@ -23,38 +23,41 @@ public class UserToLoginServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		String username=request.getParameter("username");
 		String password=request.getParameter("password");
-		String vcode=request.getParameter("vcode");
 		
 		Map<String, String> error=new HashMap<String, String>();
-		
-		String sessionVcode=(String)request.getSession().getAttribute(ValidateCode.VCODE_SESSION_KEY);
 		/*
+		String sessionVcode=(String)request.getSession().getAttribute(ValidateCode.VCODE_SESSION_KEY);
+		
 		if(!sessionVcode.equalsIgnoreCase(vcode)){
 			error.put("vcode", "验证码错误");
 		}*/
-		if(username==null || username.length()<3 || username.length()<12){
+		if(username==null || username.length()<3 || username.length()>12){
 			error.put("username", "用户名称长度介于[3,12]之间");
 		}
 		if(password==null || password.length()<3 || password.length()>12){
-			error.put("passowrd", "密码长度介于[3,12]之间");
+			error.put("password", "密码长度介于[3,12]之间");
 		}
 		
 		if(error.size()>0){
 			request.setAttribute("error", error);
 			request.getRequestDispatcher("/WEB-INF/jsp/userLogin/UserLogin.jsp").forward(request, response);
+			return;
 		}
 		
 		UserLoginDao userDao=new UserLoginDao();
-		UserLogin login=userDao.Login(username, password);
+		UserLogin login=userDao.Login(username,password);
 		if(login==null){
 			error.put("login", "用户名或密码错误");
 			request.setAttribute("error", error);
 			request.getRequestDispatcher("/WEB-INF/jsp/userLogin/UserLogin.jsp").forward(request, response);
+			return;
 		}else{
 			request.getSession().setAttribute("login", login);
-			response.sendRedirect("/WEB-INF/jsp/index.jsp");
+			response.sendRedirect("/jsp-website/userpage/article");
+			return;
 		}
 	}
 
